bb59f0949ce9d837560dc0816b273216539280e8
Katsyaryna Anikevich <anick6916@gmail.com>
2026-05-21 15:33:31 +0200

    ci bug fixed(i hope)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -6,3 +7,6 @@ SCRAPER_DIR = PROJECT_ROOT / "scraper"
 
 sys.path.insert(0, str(PROJECT_ROOT))
 sys.path.insert(0, str(SCRAPER_DIR))
+
+os.environ.setdefault("SUPABASE_URL", "https://test.supabase.co")
+os.environ.setdefault("SUPABASE_SERVICE_ROLE_KEY", "test-service-role-key")
